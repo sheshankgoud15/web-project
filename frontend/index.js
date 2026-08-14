bf29bd210fbb967318317350af932c99b00349d2
sheshankgoud15 <sheshankgoud15@gmail.com>
2026-08-14 11:00:33 +0530

    change in frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,48 @@
-alert("hello form javascript")
+//alert("hello form javascript")
+const users = [
+ {
+   "name" : "Jonn Doe",
+   "gender" : "Male",
+   "image" : "john.png"
+ },
+   {
+   "name" : "Jane Doe",
+   "gender" : "Female",
+   "image" : "jane.png"
+ }
+]
+
+
+var curIndex = 0;
+
+
+function toggle(){
+ if(curIndex == 0){
+   curIndex = 1;}
+ else{
+   curIndex = 0;
+ }
+
+ document.getElementById("card-img").src = users[curIndex].image
+ document.getElementById("card-name").innerText =users[curIndex].name
+ 
+ 
+ document.getElementById("card-gender").innerText = users[curIndex].gender
+
+}
+function random() {
+    fetch('https://randomuser.me/api')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var detail = data.results[0];
+
+            document.getElementById("card-img").src = detail.picture.large;
+            document.getElementById("card-gender").innerText = detail.gender;
+
+            var fullname = detail.name.title + " " +detail.name.first + " " + detail.name.last;
+            document.getElementById("card-name").innerText = fullname;
+        });
+}
+
